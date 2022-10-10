03fae1bb03695f16acbad7bacb9b076875f9c1ee
ramesh22ui <ramesh.kunchapu22@gmail.com>
2022-10-10 16:27:40 +0530

    added useCallBack component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UseCallBack from './components/usecallback';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React Developers</h1>
+      <UseCallBack />
     </div>
   );
 }
